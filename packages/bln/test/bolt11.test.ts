aa649f7792d479225e807aa2fa016d8c41ada18d
paluh (aka Tomasz Rybarczyk) <paluho@gmail.com>
2025-12-21 13:42:23 +0100

    Improve bolt11 parsing errors

--- packages/bln/test/bolt11.test.ts
+++ packages/bln/test/bolt11.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect, test } from "vitest";
+import { describe, it, expect } from "vitest";
 import { bolt11 } from "../src/index";
-import { base58, hex } from "@scure/base";
-import { FallbackAddress } from "../src/bolt11";
+import { hex } from "@scure/base";
+import { DecodedInvoice, FallbackAddress } from "../src/bolt11";
 import * as bitcoinjs from "bitcoinjs-lib";
 
 const PAYMENT_SECRET = hex.decode(
@@ -20,9 +20,22 @@ function parseAddress(s: string): FallbackAddress {
   throw new Error("cannot parse address");
 }
 
+function runParse(s: string): DecodedInvoice {
+  const result = bolt11.parse(s);
+  return result.match(
+    (res) => {
+      expect(res).toBeDefined();
+      return res;
+    },
+    (err) => {
+      throw err;
+    }
+  );
+}
+
 describe("bolt 11 spec", () => {
   it("example 1", () => {
-    const my = bolt11.parse(
+    const my = runParse(
       "lnbc1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdpl2pkx2ctnv5sxxmmwwd5kgetjypeh2ursdae8g6twvus8g6rfwvs8qun0dfjkxaq9qrsgq357wnc5r2ueh7ck6q93dj32dlqnls087fxdwk8qakdyafkq3yap9us6v52vjjsrvywa6rt52cm9r9zqt8r2t7mlcwspyetp5h2tztugp9lfyql",
     );
     const their = {
@@ -45,7 +58,7 @@ describe("bolt 11 spec", () => {
     expect(my.payee).toStrictEqual(their.payee);
   });
   it("example 2", () => {
-    const my = bolt11.parse(
+    const my = runParse(
       "lnbc2500u1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdq5xysxxatsyp3k7enxv4jsxqzpu9qrsgquk0rl77nj30yxdy8j9vdx85fkpmdla2087ne0xh8nhedh8w27kyke0lp53ut353s06fv3qfegext0eh0ymjpf39tuven09sam30g4vgpfna3rh",
     );
     const their = {
@@ -70,7 +83,7 @@ describe("bolt 11 spec", () => {
     expect(my.payee).toStrictEqual(their.payee);
   });
   it("example 4", () => {
-    const my = bolt11.parse(
+    const my = runParse(
       "lnbc20m1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqhp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqs9qrsgq7ea976txfraylvgzuxs8kgcw23ezlrszfnh8r6qtfpr6cxga50aj6txm9rxrydzd06dfeawfk6swupvz4erwnyutnjq7x39ymw6j38gp7ynn44",
     );
     const their = {
@@ -97,7 +110,7 @@ describe("bolt 11 spec", () => {
     expect(my.payee).toStrictEqual(their.payee);
   });
   it("example 5", () => {
-    const my = bolt11.parse(
+    const my = runParse(
       "lntb20m1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygshp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqfpp3x9et2e20v6pu37c5d9vax37wxq72un989qrsgqdj545axuxtnfemtpwkc45hx9d2ft7x04mt8q7y6t0k2dge9e7h8kpy9p34ytyslj3yu569aalz2xdk8xkd7ltxqld94u8h2esmsmacgpghe9k8",
     );
     const their = {
@@ -128,7 +141,7 @@ describe("bolt 11 spec", () => {
     expect(my.payee).toStrictEqual(their.payee);
   });
   it("example 6", () => {
-    const my = bolt11.parse(
+    const my = runParse(
       "lnbc20m1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqhp58yjmdan79s6qqdhdzgynm4zwqd5d7xmw5fk98klysy043l2ahrqsfpp3qjmp7lwpagxun9pygexvgpjdc4jdj85fr9yq20q82gphp2nflc7jtzrcazrra7wwgzxqc8u7754cdlpfrmccae92qgzqvzq2ps8pqqqqqqpqqqqq9qqqvpeuqafqxu92d8lr6fvg0r5gv0heeeqgcrqlnm6jhphu9y00rrhy4grqszsvpcgpy9qqqqqqgqqqqq7qqzq9qrsgqdfjcdk6w3ak5pca9hwfwfh63zrrz06wwfya0ydlzpgzxkn5xagsqz7x9j4jwe7yj7vaf2k9lqsdk45kts2fd0fkr28am0u4w95tt2nsq76cqw0",
     );
     const their = {
